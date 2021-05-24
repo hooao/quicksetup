64dd03042babfa613a89e8d66f713212968e8a3a
hooao <hooaosir@163.com>
2021-05-24 15:07:14 +0800

    deploy inputrc and tmux config to script.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 
 VIMRC="https://raw.githubusercontent.com/hooao/quicksetup/master/vimrc"
 VUNDLEVIM="https://github.com/VundleVim/Vundle.vim.git"
+INPUTRC="https://raw.githubusercontent.com/hooao/quicksetup/master/inputrc"
+TMUXCONFIG="https://raw.githubusercontent.com/hooao/quicksetup/master/tmux.conf"
 
 git clone --depth=1 $VUNDLEVIM ~/.vim/Vundle.vim
 
 wget -O ~/.vimrc $VIMRC
+wget -O ~/.inputrc $INPUTRC
+wget -O ~/.tmux.conf $TMUXCONFIG
 echo "done!"
