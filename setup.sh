1685e518dd0e70ade23a5428a5c009e79401a9bb
hooao <hooaosir@163.com>
2021-11-23 12:54:05 +0800

    nberc to the install script.

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ VIMRC="https://raw.githubusercontent.com/hooao/quicksetup/master/vimrc"
 VUNDLEVIM="https://github.com/VundleVim/Vundle.vim.git"
 INPUTRC="https://raw.githubusercontent.com/hooao/quicksetup/master/inputrc"
 TMUXCONFIG="https://raw.githubusercontent.com/hooao/quicksetup/master/tmux.conf"
+NBERC=“https://raw.githubusercontent.com/hooao/quicksetup/master/nberc”
 
 git clone --depth=1 $VUNDLEVIM ~/.vim/Vundle.vim
 
 wget -O ~/.vimrc $VIMRC
 wget -O ~/.inputrc $INPUTRC
 wget -O ~/.tmux.conf $TMUXCONFIG
+wget -O ~/.nberc $NBERC
 echo "done!"
